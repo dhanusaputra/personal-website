95162cf4c73561bca5bed4861ec41df0105ed496
dhanu <andreasdhanu@gmail.com>
2020-05-31 21:12:53 +0700

    Updates

---
+++ precache-manifest.0b934251a41c660245be175a1be1301d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df71221b0b04299c446b01259d26838e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d34777f50cf1179660",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "891920fcd2d677057feb",
+    "url": "/static/js/2.38bfb691.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38bfb691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d34777f50cf1179660",
+    "url": "/static/js/main.b373c319.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  }
+]);
