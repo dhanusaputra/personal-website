b51bee859c6272a836cd697692ec452d7f22d7e0
dhanu <andreasdhanu@gmail.com>
2020-05-31 20:40:57 +0700

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders something', () => {
+  const component = render(
+    <App />
+  )
+  expect(component.container).toHaveTextContent('nothing personal here')
+})
