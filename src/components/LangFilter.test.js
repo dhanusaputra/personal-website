615355b67e11475725c655e03d900f2c546d61b6
dhanu <andreasdhanu@gmail.com>
2020-12-06 09:14:58 +0700

    fix test

--- src/components/LangFilter.test.js
+++ src/components/LangFilter.test.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
+import { useSelector } from 'react-redux'
 import LangFilter from './LangFilter'
+
+const mockLangs = [{ id : 'en' }, { id: 'id' }]
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+}))
 
 describe('<LangFilter />', () => {
   let component
 
   beforeEach(() => {
+    useSelector.mockReturnValueOnce(mockLangs)
     component = render(
       <LangFilter />
     )
   })
+  afterEach(() => {
+    useSelector.mockClear()
+  })
 
-  test('renders all language', () => {
+  test('renders some languages', () => {
     expect(component.container).toHaveTextContent('ID')
     expect(component.container).toHaveTextContent('EN')
   })
