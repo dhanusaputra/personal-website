b40bd71a9c0b62933261913bf02976cc983c3139
dhanu <andreasdhanu@gmail.com>
2024-11-08 13:21:40 +0700

    upgrade - upgrade

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next/hooks'
+import { initReactI18next } from 'react-i18next'
 
 import translationID from './locales/id/translation.json'
 import translationEN from './locales/en/translation.json'
--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import langReducer from './reducers/langReducer';
 
 const store = configureStore({
   reducer: langReducer,
-  devTools: composeWithDevTools(),
 });
 
 export default store;
