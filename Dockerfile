fa1b23477f903dcf9ee0df9db6ecfb538a36ccea
dhanu <andreasdhanu@gmail.com>
2025-10-05 06:57:56 +0700

    update-doc - Update documentation

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Build Stage
+FROM node:lts-alpine AS build
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+ENV PUBLIC_URL=/
+RUN npm run build
+ 
+# Production Stage
+FROM nginx:stable-alpine AS production
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Resume from './components/Resume';
 
 const App = () => {
   return (
     <BrowserRouter basename='/personal-website'>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/resume' element={<Resume />} />
       </Routes>
     </BrowserRouter>
   );
